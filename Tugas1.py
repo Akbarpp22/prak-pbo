02bc47adc9bdb3c89afe01bba9706ae3ae3109c3
Akbarpp22 <127003012+Akbarpp22@users.noreply.github.com>
2023-03-05 14:55:48 +0700

    Update and rename Tugas1 to Tugas1.py

---
+++ Tugas1.py
@@ -0,0 +1,16 @@
+class Mahasiswa:
+    def __init__(self, nama, nim, kelas, sks):
+        self.nama = nama
+        self.nim = nim
+        self.kelas = kelas
+        self.sks = sks
+    def tampil_mahasiswa(self):
+        print("Akbar")
+        print("121140065")
+        print("RB")
+        print("20")
+
+Mahasiswa = Mahasiswa("Akbar", 121140065, "RB", 20)
+Mahasiswa.tampil_mahasiswa()
+
+   
